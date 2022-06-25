4904b492c3ec2ffb5635e00c18768739a8ae24b8
ibrahimhossain01752 <ibrahimhossain01752@gmail.com>
2022-06-25 21:28:01 +0600

    first commit

---
+++ src/Paint/Paint.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Paint = () => {
+    return (
+        <div>
+            <h1>Summer Art Camp! 5 Days of Artists and Painting Monet, Van Gosh, Matisse, & Moee</h1>
+            <p>Multi-Day Course</p>
+        </div>
+    );
+};
+
+export default Paint;
