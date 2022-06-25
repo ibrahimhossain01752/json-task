c25b6f2c6cd8e17fd1c5cabae48064ec1a7d94d9
ibrahimhossain01752 <ibrahimhossain01752@gmail.com>
2022-06-25 22:05:08 +0600

    json added

--- src/Paint/Paint.js
+++ src/Paint/Paint.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Paint.css';
 
 const Paint = () => {
+    const [paint, setPaint] = useState([]);
+    useEffect(() => {
+        fetch('artist.json')
+        .then(res => res.json())
+        .then(data => setPaint(data))
+    },[])
     return (
         <div>
-            <h1>Summer Art Camp! 5 Days of Artists and Painting Monet, Van Gosh, Matisse, & Moee</h1>
-            <p>Multi-Day Course</p>
+            <h1>Summer Art Camp! 5 Days of Artists and Painting Monet, <span class="first-span">Van Gosh, Matisse, & More</span></h1>
+            <p>Multi-Day Course a<span>&#63;</span></p>
         </div>
     );
 };
